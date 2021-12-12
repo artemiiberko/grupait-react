6d6f7c6ef9f52c77923edcdb5d4c240419715e9a
artemiiberko <artemii.berko@gmail.com>
2021-12-12 07:21:45 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import CardContent from "@mui/material/CardContent"
+import Typography from "@mui/material/Typography"
+import Card from "@mui/material/Card"
+import Button from "@mui/material/Button"
+import CardActions from "@mui/material/CardActions"
+import React, { useState } from "react"
 
 function App() {
+  function randomQuote(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1)
+    return Math.round(rand)
+  }
+
+  const [newQuote, setNewQuote] = useState({
+    author: "",
+    quote: "",
+  })
+
+  const [prevQuote, setPrevQuote] = useState({
+    author: "",
+    quote: "",
+  })
+
+  let gettingQuote = async () => {
+    setPrevQuote({
+      author: newQuote.author,
+      quote: newQuote.quote,
+    })
+    const api_url = await fetch(
+      "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
+    )
+    const quotes = await api_url.json()
+    let i = randomQuote(0, quotes.length)
+    setNewQuote({
+      author: quotes[i].author,
+      quote: quotes[i].quote,
+    })
+  }
+
+  let gettingPrevQuote = () => {
+    setNewQuote({
+      author: prevQuote.author,
+      quote: prevQuote.quote,
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Card
+        variant="outlined"
+        sx={{
+          width: "350px",
+        }}
+      >
+        <CardContent
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            minHeight: "250px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Typography
+            sx={{ fontSize: 14, color: "#666", paddingBottom: "15px" }}
+          >
+            {newQuote.author}
+          </Typography>
+          <Typography variant="h5">{newQuote.quote}</Typography>
+        </CardContent>
+        <CardActions sx={{ justifyContent: "space-around" }}>
+          <Button variant="contained" onClick={gettingPrevQuote}>
+            Previous Quote
+          </Button>
+          <Button variant="contained" onClick={gettingQuote}>
+            New Quote
+          </Button>
+        </CardActions>
+      </Card>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
